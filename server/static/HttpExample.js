a820a78fd5e96e0fed55ca28568ac3d2706d3b2a
happy <alexandre.moreau@epitech.eu>
2016-07-08 23:58:27 +0200

    Require undefined

--- server/static/HttpExample.js
+++ server/static/HttpExample.js
@@ -1,34 +1,3 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-
-function httpPost(url, data, callback)
-{
-    var xmlHttp = new XMLHttpRequest();
-
-    xmlHttp.open("POST", url, true);
-    xmlHttp.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-    xmlHttp.send(JSON.stringify(data));
-    xmlHttp.onload = callback;
-}
-
-
-function httpGet(url, callback)
-{
-    var xmlHttp = new XMLHttpRequest();
-
-    xmlHttp.open("GET", url, true);
-    xmlHttp.send();
-    xmlHttp.onload = callback;
-}
-
-function encodeQueryData(data)
-{
-    var ret = [];
-
-    for (var d in data)
-	ret.push(encodeURIComponent(d) + "=" + encodeURIComponent(data[d]));
-    return ret.join("&");
-}
-
 //USAGE
 httpGet('http://127.0.0.1:5000/list_group_doc?' + encodeQueryData({'token': 'test'}), handleResponse);
 httpGet('http://127.0.0.1:5000/list_vm?' + encodeQueryData({'token': 'test'}), handleResponse);
@@ -44,9 +13,3 @@ httpPost('http://127.0.0.1:5000/change_pwd', {'token': 'test', 'user': 'yolo', '
 httpPost('http://127.0.0.1:5000/create_account', {'token': 'test', 'user': 'test', 'pwd': 'yolo'}, handleResponse);
 httpPost('http://127.0.0.1:5000/change_auth', {'token': 'test', 'user': 'test', 'auth': 'retgy', 'group': 'auth'}, handleResponse);
 httpPost('http://127.0.0.1:5000/change_vm', {'token': 'test', 'vm': 'BSD', 'status': 'down'}, handleResponse);
-
-function handleResponse()
-{
-    var data = JSON.parse(this.responseText);
-    console.log(data);
-}
---
+++ server/static/http.js
@@ -0,0 +1,36 @@
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+function httpPost(url, data, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+
+    xmlHttp.open("POST", url, true);
+    xmlHttp.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+    xmlHttp.send(JSON.stringify(data));
+    xmlHttp.onload = callback;
+}
+
+
+function httpGet(url, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+
+    xmlHttp.open("GET", url, true);
+    xmlHttp.send();
+    xmlHttp.onload = callback;
+}
+
+function encodeQueryData(data)
+{
+    var ret = [];
+
+    for (var d in data)
+	ret.push(encodeURIComponent(d) + "=" + encodeURIComponent(data[d]));
+    return ret.join("&");
+}
+
+function handleResponse()
+{
+    var data = JSON.parse(this.responseText);
+    console.log(data);
+}
