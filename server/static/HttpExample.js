cc8b423e126f8f33e3ae53e65cad20a40ba8fd99
happy <alexandre.moreau@epitech.eu>
2016-07-09 02:03:43 +0200

    Folder done

--- server/static/HttpExample.js
+++ server/static/HttpExample.js
@@ -1,3 +1,48 @@
+function httpPost(url, data, callback)
+{
+    var request = new xmlhttprequest();
+    request.onreadystatechange = function()
+    {
+        if (request.readyState == 4 && request.status == 200)
+        {
+            callback(request.responseText); // Another callback here
+        }
+    }; 
+    request.open("POST", url, true);
+    request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+    request.send(JSON.stringify(data));
+}
+
+
+function httpGet(url, callback)
+{
+    var request = new xmlhttprequest();
+    request.onreadystatechange = function()
+    {
+        if (request.readyState == 4 && request.status == 200)
+        {
+            callback(request.responseText); // Another callback here
+        }
+    }; 
+    request.open('GET', url);
+    request.send();
+}
+
+function encodeQueryData(data)
+{
+    var ret = [];
+
+    for (var d in data)
+	ret.push(encodeURIComponent(d) + "=" + encodeURIComponent(data[d]));
+    return ret.join("&");
+}
+
+function handleResponse()
+{
+    var data = JSON.parse(this.responseText);
+    console.log(data);
+}
+
 //USAGE
 httpGet('http://127.0.0.1:5000/list_group_doc?' + encodeQueryData({'token': 'test'}), handleResponse);
 httpGet('http://127.0.0.1:5000/list_vm?' + encodeQueryData({'token': 'test'}), handleResponse);
